16236e410de2c914f5a5b38c87b8d79e807c9318
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2020-05-05 13:05:38 +0200

    module2 ex1-4

--- GJP100--modul-3/utils3.js
+++ GJP100--modul-3/utils3.js
@@ -2,11 +2,13 @@
 *   ============================== 
 * 1. DOM MANIPULATION
 * 2. FUNCTION APPLICATION
-* 3. MATH RANDOMNESS
-* 4. MATH ARITHMETIC
-* 5. HTML GENERATION
-* 6. ARRAY MANIPULATION
-* 7. PRETTYPRINT
+* 3. FUNCTION DEFINITON
+* 4. MATH RANDOMNESS
+* 5. MATH ARITHMETIC
+* 6. HTML GENERATION
+* 7. ARRAY MANIPULATION
+* 8. PRETTYPRINT
+
 *
 */
 
@@ -37,22 +39,46 @@ const fx = (f, ...xs) => (...ys) => f(...xs, ...ys);
 const fy = (f, ...xs) => (...ys) => f(...ys, ...xs);
 
 
+/**********************
+*  FUNCTION DEFINITON *
+**********************/
+
+// the cond construct as in Clojure
+// takes pairs of condition and function
+// and applies the first function for which the condition resturns true
+const cond = (...cfs) => (...xs) => {
+  for(let i = 0;i < cfs.length; i += 2) if( cfs[i](...xs) ) return cfs[i+1](...xs);
+  throw `cond: no match for input '${xs}'`;
+};
+// test for the default clause
+const otherwise = () => true;
+
+// multi-functions that dispaches on a function of all the args
+// dispatch on the dispatch function disp using the result as an index to mf
+const arity = (...fs) => (...xs) => fs[ qty(...xs) ](...xs);
 
 
-/*********************
- * MATH: RANDOMNESS  *
- ********************/
 
-// pick a random element from all args
-const pick = (...xs) => xs[Math.floor( xs.length * Math.random() )];
 
-// no  argument:       return a float between 0 inclusive and 1 non-inclusive
-// one argument n:     return a random integer between 0 and n non-inclusive
-// two arguments a, b: return a random integer between a inclusive and b non-inclusive
-const rnd = (...xs) => [rnd0, rnd1, rnd2][xs.length](...xs); // dispatch on numer of args
-  const rnd0 = ()    => Math.random(); 
-  const rnd1 = (n)   => Math.floor( n * rnd0() );
-  const rnd2 = (a,b) => a + rnd1(b-a);
+/********************
+ * MATH: RANDOMNESS *
+ *******************/
+
+// function rnd is oveloaded in four ways:
+// no  argument            : return a float between 0 inclusive and 1 non-inclusive
+// one argument <int>      : return a random integer between 0 and n non-inclusive
+// one argument <array>    : pick a random element
+// two arguments <int,int> : return a random integer between a inclusive and b non-inclusive
+
+const rnd = arity(
+  ()     => Math.random(), 
+  (x)    => cond( 
+              Number.isInteger, (int) => Math.floor( int * rnd() ),
+              Array.isArray,    (arr) => arr[Math.floor( arr.length * rnd() )],
+              otherwise,        (t)   => { throw `RND1: expected <array> or <int> but was given '${xs}'`; }
+            )(x),
+  (a, b) => a + rnd(b-a),
+);
   
   
   
@@ -86,8 +112,8 @@ const tag = (...xs) => [tag0,tag1,tag2][xs.length](...xs); // dispatch on arity
   const tag2 = (tagname, f) => (strs) => tag1(tagname)( strs.map( f ).join("\n") );
 
 // sample usage of `tag`: creating a html table
-// table2html :: [[[ html ]]] => html
-const table2html = tag("table", tag("tr", tag("td")));
+// array2html :: [[[ html ]]] => html
+const array2html = tag("table", tag("tr", tag("td")));
 
 
 
