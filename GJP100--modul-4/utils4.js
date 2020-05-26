fd7d8591cc0cf520a1f1fa0851784803c59fb0fe
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2020-05-26 17:37:03 +0200

    module 4 kodförståelse

---
+++ GJP100--modul-4/utils4.js
@@ -0,0 +1,11 @@
+/********************
+ * DOM MANIPULATION *
+ ********************/
+
+// get the elem for an id
+const id2elem = (id) => document.getElementById(id);
+// get the first elem for an query
+const query2elem = (q) => document.querySelector(q);
+// get all elems for an query as a real Array (not a node list)
+const query2elems = (q) => Array.from( document.querySelectorAll(q) );
+
